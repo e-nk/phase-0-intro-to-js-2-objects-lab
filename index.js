e020f4f609da892d9725c2c7c1707ea291c78bd5
e-nk <enocklagat747@gmail.com>
2022-11-19 13:51:36 +0300

    DONE WITH ASSIGNMENT

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var employee={
+    key:'employees',   
+}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    employee ={...employee}
+    employee['streetAddress'] = '11 Broadway'; 
+    return employee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee.name='Sam';
+    employee.streetAddress= "12 Broadway";
+    return employee;
+}   
+function deleteFromEmployeeByKey(employee, key){
+    const newEmployee={...employee};
+    delete newEmployee[key]
+    return newEmployee;
+   
+} 
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee['name'];
+    delete employee['streetAddress'];
+    return employee;
+}
